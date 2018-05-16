c338128a9f275f5fcafd0107d02a70ae9f11cb2a
Hawk Kim <hawk_kim74@hotmail.com>
2018-05-16 21:52:32 +0900

    adding button handler using GPIO ISR

--- main/lcd_driver.c
+++ main/lcd_driver.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
 #include "st7735.h"
 #include "app_wifi.h"
+#include "driver/gpio.h"
+
+#define BUTTON_1        35
+#define BUTTON_2        34
+#define BUTTON_3        39
+
+#define BUTTON_PIN_SEL    ((1ULL << BUTTON_1) | (1ULL << BUTTON_2) | (1ULL << BUTTON_3))
 
 static const char* TAG = "lcd driver";
+static xQueueHandle gpio_evt_queue = NULL;
 
-void print_page1(void)
+static void
+IRAM_ATTR gpio_isr_handler(void* arg)
+{
+  uint32_t gpio_num = (uint32_t) arg;
+
+  xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+
+static void
+print_page1(void)
 {
   tcpip_adapter_ip_info_t   info;
   bool                      is_configured;
@@ -44,8 +63,24 @@ void print_page1(void)
   }
 }
 
-void lcd_driver_task(void *pvParameter)
+static void
+print_page2(void)
 {
+  st7735_drawstring(0, 0, "Page 2               ", ST7735_WHITE);
+}
+
+static void
+print_page3(void)
+{
+  st7735_drawstring(0, 0, "Page 3               ", ST7735_WHITE);
+}
+
+static  void
+lcd_driver_task(void *pvParameter)
+{
+  uint32_t pin_num,
+           page_num = 1;
+
   st7735_initr(INITR_144GREENTAB);
   st7735_fillscreen(ST7735_BLACK);
 
@@ -56,14 +91,62 @@ void lcd_driver_task(void *pvParameter)
   
   while(1)
   {
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    print_page1();
+    if(xQueueReceive(gpio_evt_queue, &pin_num, 1000 / portTICK_PERIOD_MS))
+    {
+      switch(pin_num)
+      {
+      case BUTTON_1:
+        page_num = 1;
+        break;
+
+      case BUTTON_2:
+        page_num = 2;
+        break;
+
+      case BUTTON_3:
+        page_num = 3;
+        break;
+      }
+      st7735_fillscreen(ST7735_BLACK);
+    }
+
+    switch(page_num)
+    {
+    case 1:
+      print_page1();
+      break;
+
+    case 2:
+      print_page2();
+      break;
+
+    case 3:
+      print_page3();
+      break;
+    }
   }
 }
 
 void
 lcd_driver_init(void)
 {
+  gpio_config_t io_conf;
+
+  io_conf.intr_type     = GPIO_PIN_INTR_NEGEDGE;
+  io_conf.pin_bit_mask  = BUTTON_PIN_SEL;
+  io_conf.mode          = GPIO_MODE_INPUT;
+  io_conf.pull_down_en  = 0;
+  io_conf.pull_up_en    = 0;
+  gpio_config(&io_conf);
+
+  gpio_install_isr_service(0);
+
+  gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
+  gpio_isr_handler_add(BUTTON_1, gpio_isr_handler, (void*)BUTTON_1);
+  gpio_isr_handler_add(BUTTON_2, gpio_isr_handler, (void*)BUTTON_2);
+  gpio_isr_handler_add(BUTTON_3, gpio_isr_handler, (void*)BUTTON_3);
+
   ESP_LOGI(TAG, "starting lcd driver");
 
   xTaskCreate(lcd_driver_task, "lcd_driver_task", 4096, NULL, 5, NULL);
