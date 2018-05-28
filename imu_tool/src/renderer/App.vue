4c22dadcb805e4c670a6ca649c52729eefc226d5
Hawk Kim <hawk_kim74@hotmail.com>
2018-05-28 23:31:47 +0900

    adding ConnectDialog for IMU tool

--- imu_tool/src/renderer/App.vue
+++ imu_tool/src/renderer/App.vue
@@ -47,13 +47,12 @@
         </v-btn>
         <v-toolbar-title v-text="title"></v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn
-          icon
-          @click.native.stop="rightDrawer = !rightDrawer"
-        >
-          <v-icon>menu</v-icon>
+
+        <v-btn icon @click="showConnectDialog = true">
+          <v-icon>play_arrow</v-icon>
         </v-btn>
       </v-toolbar>
+
       <v-content>
         <v-container fluid fill-height>
           <v-slide-y-transition mode="out-in">
@@ -61,33 +60,23 @@
           </v-slide-y-transition>
         </v-container>
       </v-content>
-      <v-navigation-drawer
-        temporary
-        fixed
-        :right="right"
-        v-model="rightDrawer"
-        app
-      >
-        <v-list>
-          <v-list-tile @click.native="right = !right">
-            <v-list-tile-action>
-              <v-icon light>compare_arrows</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
+
       <v-footer :fixed="fixed" app>
         <v-spacer></v-spacer>
         <span>&copy; 2017</span>
       </v-footer>
+
+      <connect-dialog :showDialog="showConnectDialog"></connect-dialog>
     </v-app>
   </div>
 </template>
 
 <script>
+  import ConnectDialog from '@/components/ConnectDialog'
+
   export default {
     name: 'imu_tool',
+    components: { ConnectDialog },
     data: () => ({
       clipped: false,
       drawer: true,
@@ -99,7 +88,8 @@
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'IMU Tool',
+      showConnectDialog: false
     })
   }
 </script>
---
+++ imu_tool/src/renderer/components/ConnectDialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog v-model="showDialog" persistent max-wdith="400px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">Connect To IMU</span>
+      </v-card-title>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  export default {
+    name: 'ConnectDialog',
+    props: ['showDialog'],
+    watch: {
+      showDialog: function (newVal) {
+        if (newVal === true) {
+          // FIXME initialize
+        }
+      }
+    },
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
